a84882b556df7b288b3c2ae7486474f71203de00
eltonc88 <eltonchan@cal.berkeley.edu>
2015-11-14 13:36:06 -0800

    add backbone / underscore utils

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// utilities
+$.getScript("js/utils/underscore-min.js");
+$.getScript("js/utils/backbone-min.js");
+
+// application specific
+$.getScript("js/survey_engine.js");
---
+++ js/survey_engine.js
@@ -0,0 +1,15 @@
+window.SurveyEngine = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    Backbone.history.start();
+  }
+};
+
+
+Backbone.Model.extend
+$(document).ready(function(){
+  SurveyEngine.initialize();
+});
